dea3dd78773ca2412c0997a0442bc57d8bacb01f
ShwetankDohroo1 <shwetankwithdohroo@gmail.com>
2024-12-23 18:43:46 +0530

    Done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import emailjs from "emailjs-com";
 import { SectionWrapper } from "../hoc";
 import toast from 'react-hot-toast';
+import Resume from '../assets/Shwetank_Resume.pdf';
 
 const Contact = () => {
     const toastId = toast.loading('Sending email...');
@@ -32,14 +33,14 @@ const Contact = () => {
             user_email: formData.email,
             user_message: formData.message,
         };
-    
-        try{
+
+        try {
             toast.success("Email Send Successfully...", { id: toastId });
             console.log("Sending EmailJS Data:", emailData);
             await emailjs.send(serviceId, templateId, emailData, userId);
             setIsSubmitted(true);
-        } 
-        catch(err){
+        }
+        catch (err) {
             toast.error(`Failed to send mail...`, { id: toastId });
             console.error("EmailJS Error:", err);
             setError("Failed to send the message. Please try again later.");
@@ -51,26 +52,26 @@ const Contact = () => {
             <h2 className="text-3xl font-bold text-center text-gray-100 mb-6">
                 Get in Touch
             </h2>
-            {isSubmitted?(
+            {isSubmitted ? (
                 <motion.div className="text-center" initial={{ scale: 0.8 }} animate={{ scale: 1 }} transition={{ duration: 0.3 }}>
                     <h3 className="text-xl font-semibold text-green-600">
                         Thank you for reaching out!
                     </h3>
                     <p className="text-gray-100">We will get back to you soon.</p>
                 </motion.div>
-            ):(
+            ) : (
                 <form className="space-y-4" onSubmit={handleSubmit}>
                     <motion.div className="flex flex-col" whileTap={{ scale: 0.98 }}>
                         <label htmlFor="name" className="text-gray-400 font-medium mb-2">
                             Name
                         </label>
-                        <input type="text" name="name" id="name" value={formData.name} onChange={handleChange} className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" required/>
+                        <input type="text" name="name" id="name" value={formData.name} onChange={handleChange} className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" required />
                     </motion.div>
                     <motion.div className="flex flex-col" whileTap={{ scale: 0.98 }}>
                         <label htmlFor="email" className="text-gray-400 font-medium mb-2">
                             Email
                         </label>
-                        <input type="email" name="email" id="email" value={formData.email}onChange={handleChange} className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" required/>
+                        <input type="email" name="email" id="email" value={formData.email} onChange={handleChange} className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" required />
                     </motion.div>
                     <motion.div className="flex flex-col" whileTap={{ scale: 0.98 }}>
                         <label htmlFor="message" className="text-gray-400 font-medium mb-2">
@@ -78,7 +79,7 @@ const Contact = () => {
                         </label>
                         <textarea name="message" id="message" value={formData.message} onChange={handleChange} rows="4" className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" required ></textarea>
                     </motion.div>
-                    {error&&(
+                    {error && (
                         <p className="text-red-600 text-sm text-center">{error}</p>
                     )}
                     <motion.button type="submit" className="w-full bg-blue-500 text-white py-2 px-4 rounded-md font-medium hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300" whileTap={{ scale: 0.95 }}>
@@ -86,6 +87,11 @@ const Contact = () => {
                     </motion.button>
                 </form>
             )}
+            <div className="resume">
+                <motion.a href={Resume} download="Shwetank_Resume.pdf" className="w-full bg-blue-500 text-white py-2 px-4 rounded-md font-medium hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 mt-5 text-center inline-block" whileTap={{ scale: 0.95 }} >
+                    Download my Resume
+                </motion.a>
+            </div>
         </motion.div>
     );
 };
